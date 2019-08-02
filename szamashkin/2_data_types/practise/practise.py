dea8a36c2815a3b075b688af3fcef594b3e9966a
SerjZa <serj.zamashkin@gmail.com>
2019-08-02 19:31:26 +0300

    szamashkin

---
+++ szamashkin/2_data_types/practise/practise.py
@@ -0,0 +1,33 @@
+price = str(12000)
+brand = input("Your car brand is: ")
+variable = str(price + str(10))
+# added car brand name
+model = input("Your car model is: ")
+variable = str(price + str(10))
+# added car model
+color = input("Your car color is: ")
+variable = str(price + str(10))
+# added car color
+year = input("Your car year_of_production is: ")
+variable = str(price + str(10))
+# added year_of_production
+engine_volume = input("Your car engine_volume is: ")
+variable = str(price + str(10))
+# added engine_volume
+odometer = input("Your car odometer info is: ")
+variable = str(price + str(10))
+# added odometer info
+phone_number = input("Your phone_number is: ")
+variable = str(price + str(10))
+# added user phone number
+print (price)
+
+
+
+
+
+
+
+
+
+
