89f5de03798c0a952ded4cb219760b980fd379ce
vsevolod.savchenko@gmail.com <vsevolod.savchenko@gmail.com>
2019-08-09 14:00:45 +0300

    Add 4th homework

---
+++ VsevolodSavchenko/4_iterations/hw/4thHW.py
@@ -0,0 +1,24 @@
+### 1
+s = input("Input something")
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+    else:
+        pass
+print("Letters", l)
+print("Digits", d)
+### 2
+for fizzbuzz in range(50):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
