d8eeac548eecfa30f244c5f0dd099ea90dc86f7b
azh2303 <ar.zhuch@gmail.com>
2019-08-09 13:33:49 +0300

    Adding iterations practise

---
+++ artem_zhuchenko/4_iterations/practise/artem_zhuchenko_iterations_practise_task3.py
@@ -0,0 +1,7 @@
+'''3. Write a Python program that accepts a sequence of lines (blank line to terminate) as input
+and prints the lines as output (all characters in lower case).'''
+
+while True:
+    user_input = input("smth: ")
+    if user_input == '':
+        break
