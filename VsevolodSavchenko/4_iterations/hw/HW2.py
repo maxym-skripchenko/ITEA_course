cfb2d987c2abe919007dbd50aeb33cf43a81d1ce
vsevolod.savchenko@gmail.com <vsevolod.savchenko@gmail.com>
2019-08-10 11:03:58 +0300

    Separate tasks

--- VsevolodSavchenko/4_iterations/hw/HW2.py
+++ VsevolodSavchenko/4_iterations/hw/HW2.py
@@ -1,15 +1,3 @@
-### 1
-s = input("Input something")
-d=l=0
-for c in s:
-    if c.isdigit():
-        d=d+1
-    elif c.isalpha():
-        l=l+1
-    else:
-        pass
-print("Letters", l)
-print("Digits", d)
 ### 2
 for fizzbuzz in range(50):
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
