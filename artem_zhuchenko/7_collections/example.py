f84d77aafc9b40bc41e8ebd3339d5737b07a86f5
Artem Zhuchenko <ar.zhuch@gmail.com>
2019-08-20 09:41:21 +0300

    updating all

---
+++ artem_zhuchenko/7_collections/example.py
@@ -0,0 +1,21 @@
+# user = {
+#     'name': "Taras",
+#     'age': 17,
+#     'address': "Kyiv"
+# }
+
+# user = dict(
+#         [
+#             ("name", "Taras"),
+#             ("age", 17),
+#             ("address", 17),
+#         ]
+# )
+
+person = {}
+
+person['name'] = "Taras"
+person["age"] = 17
+person["from"] = "Kyiv"
+
+print(person)
