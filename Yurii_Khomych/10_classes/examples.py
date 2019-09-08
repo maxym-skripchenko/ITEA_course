471e9bc345cc443660f5dc7138aed6204358ed31
yuriikhomych <yuriykhomich@gmail.com>
2019-09-08 19:03:59 +0300

    Update examples.py for 10_classes lesson

--- Yurii_Khomych/10_classes/examples.py
+++ Yurii_Khomych/10_classes/examples.py
@@ -101,61 +101,58 @@
 # sara.breed
 # # 'Boston Terrier'
 
-
-# Parent class
-class Dog:
-
-    # Class attribute
-    species = 'mammal'
-
-    # Initializer / Instance attributes
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    # instance method
-    def description(self):
-        return "{} is {} years old".format(self.name, self.age)
-
-    # instance method
-    def speak(self, sound):
-        return "{} says {}".format(self.name, sound)
-
-
-# Child class (inherits from Dog class)
-class RussellTerrier(Dog):
-    def run(self, speed):
-        if speed in ("slow", "fast"):
-            return "{} runs {}".format(self.name, speed)
-
-
-# Child class (inherits from Dog class)
-class Bulldog(Dog):
-    def run(self, speed):
-        return "{} runs {}".format(self.name, speed)
-
-    def description(self):
-        return "{} is {} years old".format(self.name, self.age).upper()
-
-
-class Dvorniaga(Bulldog, RussellTerrier):
-    species = 'reptile'
-
-
-# Child classes inherit attributes and
-# behaviors from the parent class
-sharik = Bulldog("Sharik", 12)
-print(sharik.description())
-
-# Child classes have specific attributes
-# and behaviors as well
-print(sharik.run("slowly"))
-
-bobik = RussellTerrier("bobik", 10)
-
-donatello = Dvorniaga(name="Donatello", age=1)
-
-donatello.run("rapidly")
-
-
-
+#
+# # Parent class
+# class Dog:
+#
+#     # Class attribute
+#     species = 'mammal'
+#
+#     # Initializer / Instance attributes
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     # instance method
+#     def description(self):
+#         return "{} is {} years old".format(self.name, self.age)
+#
+#     # instance method
+#     def speak(self, sound):
+#         return "{} says {}".format(self.name, sound)
+#
+#
+# # Child class (inherits from Dog class)
+# class RussellTerrier(Dog):
+#     def run(self, speed):
+#         if speed in ("slow", "fast"):
+#             return "{} runs {}".format(self.name, speed)
+#
+#
+# # Child class (inherits from Dog class)
+# class Bulldog(Dog):
+#     def run(self, speed):
+#         return "{} runs {}".format(self.name, speed)
+#
+#     def description(self):
+#         return "{} is {} years old".format(self.name, self.age).upper()
+#
+#
+# class Dvorniaga(Bulldog, RussellTerrier):
+#     species = 'reptile'
+#
+#
+# # Child classes inherit attributes and
+# # behaviors from the parent class
+# sharik = Bulldog("Sharik", 12)
+# print(sharik.description())
+#
+# # Child classes have specific attributes
+# # and behaviors as well
+# print(sharik.run("slowly"))
+#
+# bobik = RussellTerrier("bobik", 10)
+#
+# donatello = Dvorniaga(name="Donatello", age=1)
+#
+# donatello.run("rapidly")
