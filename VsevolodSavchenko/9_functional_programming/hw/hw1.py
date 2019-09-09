24680d3261404e191a3e7b517bf593c63b46443b
vsevolod.savchenko@gmail.com <vsevolod.savchenko@gmail.com>
2019-09-09 14:11:49 +0300

    Add 9th hw

---
+++ VsevolodSavchenko/9_functional_programming/hw/hw1.py
@@ -0,0 +1,18 @@
+import re
+
+
+def make_3sg_form(verb):
+    es = ("o", "ch", "s", "sh", "x", "z")
+    if verb.endswith("y"):
+        return re.sub("y$", "ies", verb)
+    elif verb.endswith(es):
+        return verb + "es"
+    else:
+        return verb + "s"
+
+
+if __name__ == "__main__":
+    print(make_3sg_form("try"))
+    print(make_3sg_form("go"))
+    print(make_3sg_form("catch"))
+    print(make_3sg_form("fix"))
