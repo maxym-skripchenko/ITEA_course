3c1f55ad21b87cf58ee210f9e393cb834b25a36e
vsevolod.savchenko@gmail.com <vsevolod.savchenko@gmail.com>
2019-09-13 12:45:12 +0300

    Redid 10th hw1 and hw2

--- VsevolodSavchenko/10_classes/hw/hw1.py
+++ VsevolodSavchenko/10_classes/hw/hw1.py
@@ -4,26 +4,9 @@ class Pets:
     def __init__(self, dogs):
         self.dogs = dogs
 
-    cats = []
-
-    def __int__(self, cats):
-        self.cats = cats
-
 
 class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.is_hungry = True
 
-    def description(self):
-        return self.name, self.age
-
-    def eat(self):
-        self.is_hungry = False
-
-
-class Cat:
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -44,23 +27,15 @@ class Sobaken_2(Dog):
     pass
 
 
-class Kotyara(Cat):
-    pass
-
-
-class Kotyara_2(Cat):
-    pass
-
-
-my_animals = [
+my_dogs = [
     Sobaken("Pes", 6),
     Sobaken_2("Pes2", 9),
-    Kotyara("Kot", 1),
-    Kotyara_2("Kot2", 2)
+    Dog("Pes3", 29)
 ]
 
-my_pets = Pets(my_animals)
-print("I have {} animals.".format(len(my_pets.dogs)))
+my_pets = Pets(my_dogs)
+
+print("I have {} dogs.".format(len(my_pets.dogs)))
 for dog in my_pets.dogs:
     dog.eat()
     print("{} is {}.".format(dog.name, dog.age))
@@ -69,16 +44,8 @@ are_my_dogs_hungry = True
 for dog in my_pets.dogs:
     if dog.is_hungry:
         are_my_dogs_hungry = True
+
 if are_my_dogs_hungry:
     print("My dogs are hungry.")
 else:
     print("My dogs are not hungry.")
-
-are_my_cats_hungry = False
-for cat in my_pets.cats:
-    if cat.is_hungry:
-        are_my_cats_hungry = True
-if are_my_cats_hungry:
-    print("My cats are hungry.")
-else:
-    print("My cats are not hungry.")
--- VsevolodSavchenko/10_classes/hw/hw2.py
+++ VsevolodSavchenko/10_classes/hw/hw2.py
@@ -8,17 +8,9 @@ class Pets:
         for dog in self.dogs:
             print(dog.walk())
 
-    cats = []
-
-    def __int__(self, cats):
-        self.cats = cats
-
-    def walk(self):
-        for dog in self.dogs:
-            print(dog.walk())
-
 
 class Dog:
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -27,55 +19,25 @@ class Dog:
     def description(self):
         return self.name, self.age
 
-    def eat(self):
-        self.is_hungry = False
-
     def walk(self):
-        return "%s is walking!" % self.name
-
-
-class Cat:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.is_hungry = True
-
-    def description(self):
-        return self.name, self.age
-
-    def eat(self):
-        self.is_hungry = False
-
-    def walk(self):
-        return "%s is walking!" % self.name
+        return "%s is walking!" % (self.name)
 
 
 class Sobaken(Dog):
-    def run(self, speed):
-        return "%s runs %s" % (self.name, speed)
+    pass
 
 
 class Sobaken_2(Dog):
-    def run(self, speed):
-        return "%s runs %s" % (self.name, speed)
+    pass
 
 
-class Kotyara(Cat):
-    def run(self, speed):
-        return "%s runs %s" % (self.name, speed)
-
-
-class Kotyara_2(Cat):
-    def run(self, speed):
-        return "%s runs %s" % (self.name, speed)
-
-
-my_animals = [
+my_dogs = [
     Sobaken("Pes", 6),
     Sobaken_2("Pes2", 9),
-    Kotyara("Kot", 1),
-    Kotyara_2("Kot2", 2)
+    Dog("Pes3", 29)
 ]
 
-my_pets = Pets(my_animals)
+my_pets = Pets(my_dogs)
+
+print("I have {} dogs.".format(len(my_pets.dogs)))
 my_pets.walk()
